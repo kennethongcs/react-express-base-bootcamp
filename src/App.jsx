cabf3e2ba6abedc5b1bf05bda09f4a359d88c81e
kennethongcs <kenneth.ongcs@gmail.com>
2022-07-19 21:37:24 +0800

    add guess-the-word base

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+const secretWord = ['b', 'a', 'n', 'a', 'n', 'a'];
+
+const InputFunction = () => {
+  // the array begins with only _
+  const [guessedLetters, setGuessedLetters] = useState([
+    '_',
+    '_',
+    '_',
+    '_',
+    '_',
+    '_',
+  ]);
+  console.log('secretWord:', secretWord);
+  console.log('guessedLetters:', guessedLetters);
+  const handleInput = (event) => {
+    const input = event.target.value.slice(-1);
+    console.log('input:', input);
+
+    // copy of guessedLetters
+    const arr = [...guessedLetters];
+
+    // loop through input
+    secretWord.forEach((letter, index) => {
+      if (letter === input) {
+        arr[index] = input;
+      }
+    });
+    setGuessedLetters(arr);
+  };
+  console.log('guessedLetters:', guessedLetters);
+  return (
+    <div>
+      <div>
+        Guess the word:{' '}
+        {guessedLetters.map((letter) => (
+          <span>{letter}</span>
+        ))}
+      </div>
+      <input onChange={handleInput} />
+    </div>
+  );
+};
 
 export default function App() {
-  return <div>This is App.jsx</div>;
+  return <InputFunction />;
 }
