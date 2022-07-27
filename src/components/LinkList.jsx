993c24025d5923bb62155a7d937c9838e86cb849
kennethongcs <kenneth.ongcs@gmail.com>
2022-07-27 11:25:39 +0800

    refactored code

--- src/components/LinkList.jsx
+++ src/components/LinkList.jsx
@@ -1,18 +1,9 @@
 import React from 'react';
 
-const ListOfLinks = ({ links, handleCheck }) => {
-  const loop = links.map((link) => (
+export default function LinkList({ links, handleCheck }) {
+  return links.map((link) => (
     <li key={`${link} + 1`}>
       {link} <input type="checkbox" onChange={handleCheck}></input>
     </li>
   ));
-  return loop;
-};
-
-export default function LinkList({ links, handleCheck }) {
-  return (
-    <div>
-      <ListOfLinks links={links} handleCheck={handleCheck} />
-    </div>
-  );
 }
